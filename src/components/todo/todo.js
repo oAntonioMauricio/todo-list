d97827af8c9ccdad8fbf9ce8b86a0ebf362a9b1f
Antonio Mauricio <oantoniomauricio@gmail.com>
2023-05-10 20:17:54 +0100

    abstract todo build flow

--- src/components/todo/todo.js
+++ src/components/todo/todo.js
@@ -7,32 +7,44 @@ const todo = (() => {
     console.log("todo ONLINE");
     const container = document.getElementById("mainContainer");
 
+    // func to build html
+    const buildHtml = (className, element, parent) => {
+      const newEl = document.createElement(element);
+      newEl.classList.add(className);
+      parent.append(newEl);
+      return newEl;
+    };
+
     // build grid for todo bars
-    const todoGrid = document.createElement("div");
-    todoGrid.classList.add("todoGrid");
-    container.append(todoGrid);
+    const todoGrid = buildHtml("todoGrid", "div", container);
 
     // build todo tab
-    const todoTab = document.createElement("div");
-    todoTab.classList.add("todoTab");
-    todoGrid.append(todoTab);
+    const todoTab = buildHtml("todoTab", "div", todoGrid);
 
     // build h3 for todo tab
-    const todoH3 = document.createElement("h4");
-    todoH3.classList.add("todoH3");
-    todoH3.textContent = "To Do";
-    todoTab.append(todoH3);
+    const todoH4 = buildHtml("todoH4", "h4", todoTab);
+    todoH4.textContent = "To Do";
 
     // build flex for todo cards
-    const todoFlex = document.createElement("div");
-    todoFlex.classList.add("todoFlex");
-    todoTab.append(todoFlex);
+    const todoFlex = buildHtml("todoFlex", "div", todoTab);
 
     // build first todo
-    const todoEl = document.createElement("div");
-    todoEl.classList.add("todoEl");
-    todoEl.textContent = "This is your first task";
-    todoFlex.append(todoEl);
+    const todoEl = buildHtml("todoEl", "div", todoFlex);
+    todoEl.textContent = "This is your first task.";
+
+    // build done tab
+    const doneTab = buildHtml("todoTab", "div", todoGrid);
+
+    // build h3 for todo tab
+    const doneH4 = buildHtml("todoH4", "h4", doneTab);
+    doneH4.textContent = "Done";
+
+    // build flex for todo cards
+    const doneFlex = buildHtml("todoFlex", "div", doneTab);
+
+    // build first todo
+    const doneEl = buildHtml("todoEl", "div", doneFlex);
+    doneEl.textContent = "This task is done!";
   };
 
   return { build };
